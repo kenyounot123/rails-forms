0c95ba3fe8d0d426ddc702f92fc8e179e910a010
kenyounot123 <kenlu519@gmail.com>
2023-10-22 22:22:00 -0400

    Finish form with rails #form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
 
     def new 
+        @user = User.new
+    end
+    def edit
+        @user = User.find(params[:id])
     end
 
     def create
-        #@user = User.new(mail: params[:email], password: params[:password])
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
         @user = User.new(user_params)
 
         if @user.save
@@ -13,10 +17,21 @@ class UsersController < ApplicationController
             render :new, status: :unprocessable_entity
         end
     end
+    
+    def update
+        @user = User.find(params[:id])
+    
+        if @user.update(user_params)
+          redirect_to @user
+        else
+          render :edit, status: :unprocessable_entity
+        end
+    
+    end
 
     private
 
     def user_params
-        params.require(:users).permit(:username,:password,:email)
+        params.require(:user).permit(:username,:password,:email)
     end
 end
